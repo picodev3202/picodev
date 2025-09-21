22f23397d987c8034cf1515dd667a5c8b1f424d0
pico dev <picodev3202@outlook.com>
2025-09-21 05:37:52 +0300

    ReadNoteBook.kt

--- src/appInit/plus/quick_code_plus/kt/ReadNoteBook.kt
+++ src/appInit/plus/quick_code_plus/kt/ReadNoteBook.kt
@@ -1,21 +1,76 @@
+import com.fasterxml.jackson.annotation.JsonAnySetter
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.jsoup.Jsoup
 
 object ReadNoteBook : MainObject() {
+
+    fun noteBookCellValue(rawValue: Any?): String {
+        if (rawValue is String) {
+            return rawValue
+        } else if (rawValue is List<*>) {
+            return rawValue.joinToString("") { it.toString() }
+        }
+        return ""
+    }
+
+    class CellSourceValueByPropertyName() {
+        operator fun getValue(thisRef: Any?, property: kotlin.reflect.KProperty<*>): String {
+            var result = ""
+            if (thisRef is Node) {
+                result = noteBookCellValue(thisRef.otherProperties[property.name])
+                // data class Debug(val _source: Any, val _sourceRaw: Any?)
+                // thisRef._debug = Debug(result, thisRef.otherProperties["source"])
+            }
+            return result
+        }
+    }
+
+    abstract class Node {
+        // @Suppress("PropertyName")  var _debug = Any()
+
+        @JsonIgnore
+        val otherProperties = mutableMapOf<String, Any?>()
+
+        @JsonAnySetter
+        @Suppress("unused")
+        fun setOtherProperty(name: String, value: Any?) {
+            otherProperties[name] = value
+        }
+    }
+
+    class NoteBook : Node() {
+        class Cell : Node() {
+            val source: String by CellSourceValueByPropertyName()
+
+            @JsonProperty("cell_type")
+            val cellType = ""
+
+            override fun toString(): String {
+                return cellType
+            }
+        }
+
+        val cells = listOf<Cell>()
+    }
 
     override fun main(args: Args) {
         println("$objectName.main " + args.devProject.name)
         println("$objectName.main " + args.devProject.rootPlace)
 
         val notebook = args.devProject.src.place("appInit/plus/tool/char_transform_debug/kt/debug.ipynb")
-        println("$objectName.main $notebook")
+        println("$objectName.main ${notebook.path}")
+
+        val notebookText = notebook.readText()
 
         val mapper = ObjectMapper()
-        val map = mapper.readValue(notebook.readText(), object : TypeReference<MutableMap<String, Any?>>() {})
+        val map = mapper.readValue(notebookText, object : TypeReference<Map<String, Any?>>() {})
+        val notebookContent = mapper.readValue(notebookText, NoteBook::class.java)
+        // notebookContent.cells.forEach { it.source }
 
         println("$objectName.main $map")
-        println("$objectName.main " + Jsoup.parse("<html/>"))
+        println("$objectName.main $notebookContent")
     }
 
     @JvmStatic
