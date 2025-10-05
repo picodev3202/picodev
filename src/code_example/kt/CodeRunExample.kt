7e1d16b39804038341723e0d132ac1531b50dc94
pico dev <picodev3202@outlook.com>
2025-10-05 23:45:42 +0300

    CodeRunExample.kt

---
+++ src/code_example/kt/CodeRunExample.kt
@@ -0,0 +1,27 @@
+object CodeRunExample {
+    val logTag = Val.logTag
+
+    object Val : MainObject() {
+        val logTag by lazy { detectLogTag() }
+        val thisFile by lazy { lookupSrcFileByClassName() }
+        val devProject by lazy { DevProjectLookup.fromCurrentDir() }
+    }
+
+    /*@formatter:off*/ object Go001 {@JvmStatic fun main(args: Array<String>) = code01( ) }
+    /*@formatter:off*/ object Go002 {@JvmStatic fun main(args: Array<String>) = code02( ) }
+    //@formatter:on
+
+    private fun code01() {
+        println("$logTag.code01")
+        println("$logTag.code01 ${Val.devProject}")
+        println("$logTag.code01 ${Val.logTagName}")
+        println("$logTag.code01 ${Val.objectName}")
+    }
+
+    private fun code02() {
+        println("$logTag.code02")
+        println("$logTag.code02 $logTag")
+        println("$logTag.code02 ${Val.logTag}")
+        println("$logTag.code02 ${Val.thisFile}")
+    }
+}
