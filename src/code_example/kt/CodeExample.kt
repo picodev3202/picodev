1766443f8a6ecc9c48f879a1f10a7242e57014a4
pico dev <picodev3202@outlook.com>
2025-10-05 22:44:01 +0300

    CodeExample.kt

---
+++ src/code_example/kt/CodeExample.kt
@@ -0,0 +1,14 @@
+object CodeExample {
+
+    /*@formatter:off*/ object Go001 {@JvmStatic fun main(args: Array<String>) = code01( ) }
+    /*@formatter:off*/ object Go002 {@JvmStatic fun main(args: Array<String>) = code02( ) }
+    //@formatter:on
+
+    private fun code01() {
+        println("CodeExample.code01")
+    }
+
+    private fun code02() {
+        println("CodeExample.code02")
+    }
+}
